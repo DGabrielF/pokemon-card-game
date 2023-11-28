778637eb21dc4ae0dc77a98c53ef50af1d03b8bb
gabriel <dgabrielfof@gmail.com>
2023-11-27 23:55:08 -0300

    feat: detail system

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -5,6 +5,7 @@ const state = {
       enemy: document.getElementById("enemy-score"),
     },
     detail: {
+      card: document.querySelector(".card-detail"),
       name: document.getElementById("card-name"),
       image: document.getElementById("card-image"),
       height: document.querySelector(".height"),
@@ -15,7 +16,7 @@ const state = {
       attack: document.querySelector(".atk"),
       defense: document.querySelector(".def"),
     },
-    fieldCards: {
+    fieldCard: {
       player: document.querySelector(".your-poke-card"),
       enemy: document.querySelector(".enemy-poke-card"),
     },
@@ -73,29 +74,68 @@ async function cardInHandGenerator() {
   }
   return cardInHandList
 }
+function handleDetails(card) {
+  state.view.detail.name.textContent = card.name;
+  state.view.detail.height.textContent = `A:${card.height}`;
+  state.view.detail.weight.textContent = `P:${card.weight}`;
+  state.view.detail.types.innerHTML = "";
+  state.view.detail.card.classList = ["card-detail"],
+  card.types.forEach((type) => {
+    state.view.detail.card.classList.add(type)
+    const typeDiv = document.createElement('div');
+    typeDiv.classList.add('type');
+    typeDiv.textContent = type;
+    state.view.detail.types.appendChild(typeDiv);
+  });
+  state.view.detail.hp.textContent = card.hp;
+  state.view.detail.speed.textContent = card.speed;
+  state.view.detail.attack.textContent = card.attack;
+  state.view.detail.defense.textContent = card.defense;
+  state.view.detail.image.src = card.image;
+  state.view.detail.image.alt = card.name;
+}
+function handleInField() {
+  state.view.fieldCard.player.innerHTML = "";
 
+  const name = document.createElement("p");
+  name.textContent = state.view.detail.name.textContent;  
+  state.view.fieldCard.player.appendChild(name)
+  console.log(state.view.detail)
+  
+  const image = document.createElement("img");
+  image.src = state.view.detail.image.src;
+  image.alt = state.view.detail.name.textContent;
+  state.view.fieldCard.player.appendChild(image)
+
+  // filtrar a lista de card na mão para encontrar o que possui o mesmo nome do detail e colar no infiel
+  // limpar o infield no final de cada rodada
+}
 async function init() {
   state.values.enemyPokemonList = await cardInHandGenerator();
 
   for (let i=0; i<state.values.enemyPokemonList.length; i++) {
     const card = document.createElement("div");
-    card.classList.add("your-card")
+    card.name = state.values.enemyPokemonList[i].name;
+    card.classList.add("your-card");
 
     const name = document.createElement("p");
-    name.textContent = state.values.enemyPokemonList[i].name
+    name.textContent = state.values.enemyPokemonList[i].name;
 
     const image = document.createElement("img");
     image.src = state.values.enemyPokemonList[i].image;
     image.alt = state.values.enemyPokemonList[i].name;
 
     state.values.enemyPokemonList[i].types.forEach((type) => {
-      card.classList.add(type)
+      card.classList.add(type);
     })
+    card.onclick = e => handleDetails(state.values.enemyPokemonList[i]);
+  
     card.appendChild(name);
     card.appendChild(image);
 
-    state.view.cardAreas.player.appendChild(card)
+    state.view.cardAreas.player.appendChild(card);
   }
+  state.action.pokemonSelector.addEventListener("click",e => handleInField());
   // state.action.pokemonSelector.addEventListener("click", e => {cardInHandGenerator(6)});
   // console.log(state)
 }
