1db705d0db8c8829713c52c966549269a7a9efb8
gabriel <dgabrielfof@gmail.com>
2023-11-28 23:54:12 -0300

    feat: restart page

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -30,10 +30,23 @@ const state = {
     },
     attribute: {
       selected: document.getElementsByName("option"),
+      duel: document.querySelector(".attribute-selected"),
+    },
+    svg: {
+      height: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#ffffff" viewBox="0 0 256 256"><path d="M167.39,196.94a8,8,0,0,1-1.73,8.72l-32,32a8,8,0,0,1-11.32,0l-32-32A8,8,0,0,1,96,192h24V64H96a8,8,0,0,1-5.66-13.66l32-32a8,8,0,0,1,11.32,0l32,32A8,8,0,0,1,160,64H136V192h24A8,8,0,0,1,167.39,196.94Z"></path></svg>`,
+      weight: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#ffffff" viewBox="0 0 256 256"><path d="M244,116V88a20,20,0,0,0-20-20H208V64a20,20,0,0,0-20-20H164a20,20,0,0,0-20,20v52H112V64A20,20,0,0,0,92,44H68A20,20,0,0,0,48,64v4H32A20,20,0,0,0,12,88v28a12,12,0,0,0,0,24v28a20,20,0,0,0,20,20H48v4a20,20,0,0,0,20,20H92a20,20,0,0,0,20-20V140h32v52a20,20,0,0,0,20,20h24a20,20,0,0,0,20-20v-4h16a20,20,0,0,0,20-20V140a12,12,0,0,0,0-24ZM36,164V92H48v72Zm52,24H72V68H88Zm96,0H168V68h16Zm36-24H208V92h12Z"></path></svg>`,
+      hp: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" viewBox="0 0 256 256"><path d="M217.36,133.36,128,224,38.64,133.36a50,50,0,0,1,70.72-70.72L128,80l18.64-17.36a50,50,0,1,1,70.72,70.72Z" opacity="0.2"></path><path d="M223,57a58.07,58.07,0,0,0-81.92-.1L128,69.05,114.91,56.86A58,58,0,0,0,33,139l89.35,90.66a8,8,0,0,0,11.4,0L223,139a58,58,0,0,0,0-82Zm-11.35,70.76L128,212.6,44.3,127.68a42,42,0,0,1,59.4-59.4l.2.2,18.65,17.35a8,8,0,0,0,10.9,0L152.1,68.48l.2-.2a42,42,0,1,1,59.36,59.44Z" opacity="0.4"></path></svg>`,
+      speed: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" viewBox="0 0 256 256"><path d="M248,192v8a8,8,0,0,1-8,8H147.31a8,8,0,0,1-5.65-2.34l-107.32-104a8,8,0,0,1,0-11.32l64-64.13a8,8,0,0,1,11.17.13l8.23,8.23a8,8,0,0,1,2.32,5.28c1.36,27.59,21.35,45.66,48.66,47.86a8,8,0,0,1,7.27,8V120a40,40,0,0,0,40,40h0A32,32,0,0,1,248,192Z" opacity="0.2"></path><path d="M216,152a32,32,0,0,1-32-32V95.7a16,16,0,0,0-14.63-15.94c-24.35-2-40.18-17.39-41.31-40.27h0A16,16,0,0,0,123.4,29l-8.22-8.23a15.91,15.91,0,0,0-22.35-.27l-.15.14-64,64.12a16,16,0,0,0,0,22.62l.09.09,107.27,104A15.93,15.93,0,0,0,147.31,216H240a16,16,0,0,0,16-16v-8A40,40,0,0,0,216,152Zm24,48H147.31l-.09-.08L40,96l63.87-64,8.21,8.2v0c.76,15.42,6.65,28.85,17,38.83,10,9.6,23.45,15.34,38.88,16.6V112H152a8,8,0,1,0,0,16h16.68a47.64,47.64,0,0,0,5.78,16H160a8,8,0,0,0,0,16h29.51A47.67,47.67,0,0,0,216,168a24,24,0,0,1,24,24ZM64,184H32a8,8,0,0,1,0-16H64a8,8,0,0,1,0,16Zm40,24a8,8,0,0,1-8,8H48a8,8,0,0,1,0-16H96A8,8,0,0,1,104,208Z" opacity="0.4"></path></svg>`,
+      attack: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" viewBox="0 0 256 256"><path d="M141.66,201,129,213.66a8,8,0,0,1-11.32,0L92,188,58.35,221.66a8,8,0,0,1-11.32,0L34.34,209a8,8,0,0,1,0-11.31L68,164,42.34,138.36a8,8,0,0,1,0-11.32L55,114.34a8,8,0,0,1,11.32,0l75.3,75.3A8,8,0,0,1,141.66,201Z" opacity="0.2"></path><path d="M216,32H152a8,8,0,0,0-6.34,3.12l-64,83.21L72,108.69a16,16,0,0,0-22.64,0l-12.69,12.7a16,16,0,0,0,0,22.63l20,20-28,28a16,16,0,0,0,0,22.63l12.69,12.68a16,16,0,0,0,22.62,0l28-28,20,20a16,16,0,0,0,22.64,0l12.69-12.7a16,16,0,0,0,0-22.63l-9.64-9.64,83.21-64A8,8,0,0,0,224,104V40A8,8,0,0,0,216,32ZM52.69,216,40,203.32l28-28L80.68,188Zm70.61-8L48,132.71,60.7,120,136,195.31ZM208,100.06l-81.74,62.88L115.32,152l50.34-50.34a8,8,0,0,0-11.32-11.31L104,140.68,93.07,129.74,155.94,48H208Z" opacity="0.4"></path></svg>`,
+      defense:`<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" viewBox="0 0 256 256"><path d="M216,56v58.77c0,84.18-71.31,112.07-85.54,116.8a7.54,7.54,0,0,1-4.92,0C111.31,226.86,40,199,40,114.79V56a8,8,0,0,1,8-8H208A8,8,0,0,1,216,56Z" opacity="0.2"></path><path d="M208,40H48A16,16,0,0,0,32,56v58.77c0,89.61,75.82,119.34,91,124.39a15.53,15.53,0,0,0,10,0c15.2-5.05,91-34.78,91-124.39V56A16,16,0,0,0,208,40Zm0,74.79c0,78.42-66.35,104.62-80,109.18-13.53-4.51-80-30.69-80-109.18V56l160,0Z" opacity="0.4"></path></svg>`,
+    },
+    result: {
+      page: document.querySelector(".continue-page"),
+      message: document.querySelector(".result"),
     }
   },
   values: {
-    numberOfCards: 2,
+    numberOfCards: 6,
     roundsPlayed: 0,
     playerScore: 0,
     playerPokemonList: null,
@@ -48,6 +61,7 @@ const state = {
   action: {
     attributeSelector: document.querySelector(".confirm-attribute"),
     pokemonSelector: document.querySelector(".confirm-pokemon"),
+    continueButton: document.querySelector(".restart")
   }
 }
 async function getPokemonData(pokemonId) {
@@ -147,8 +161,8 @@ function handleInField() {
 function enemyPokeInField() {
   state.view.fieldCard.enemy.innerHTML = "";
   const cards = Array.from(document.querySelectorAll(`.enemy-card`));
-  const randeomId = Math.floor(Math.random()*cards.length);
-  const card = cards[randeomId];
+  const randomId = Math.floor(Math.random()*cards.length);
+  const card = cards[randomId];
   console.log(card.classList)
 
   state.view.fieldCard.enemy.innerHTML = card.innerHTML;
@@ -156,7 +170,12 @@ function enemyPokeInField() {
   state.view.fieldCard.enemy.classList.remove ("enemy-card");
   state.view.fieldCard.enemy.classList.add ("your-card");
 
-  state.values.enemyPokemonInField = state.values.enemyPokemonList[randeomId]
+  state.values.enemyPokemonInField = state.values.enemyPokemonList[randomId]
+}
+function randomAttribute() {
+  const randomId = Math.floor(Math.random()*state.values.attributes.length);
+  state.values.attributeSelected = state.values.attributes[randomId];
+  state.view.attribute.duel.innerHTML = state.view.svg[state.values.attributes[randomId]];
 }
 function whoStarts() {
   // state.values.playingOrder = (Math.random()>.5)?["player", "enemy"]:["enemy", "player"];
@@ -169,8 +188,8 @@ function handleTurn() {
   
   if (state.values.playingOrder[state.values.roundsPlayed % state.values.playingOrder.length] === "enemy") {
     state.action.attributeSelector.disabled = true;
+    randomAttribute();
     enemyPokeInField();
-    state.values.attributeSelected = state.values.attributes[Math.floor(Math.random()*state.values.attributes.length)];
   } else if (state.values.playingOrder[state.values.roundsPlayed % state.values.playingOrder.length] === "player") {
     const optionUnchecked = document.querySelectorAll('input[type="radio"]');
     optionUnchecked.forEach(option => option.disabled = false);
@@ -205,17 +224,20 @@ function handleAttribute() {
   optionUnchecked.forEach(option => option.disabled = true);
 
   state.values.attributeSelected = option.value;
-
+  
   state.action.attributeSelector.disabled = true;
 
+  state.view.attribute.duel.innerHTML = state.view.svg[option.value];
   checkComparisonRquirements()
 }
 function endGame() {
   if (state.values.playerScore>=state.values.numberOfCards) {
-    console.log("Parabéns, você venceu!")
+    state.view.result.page.style.display = "flex";
+    state.view.result.message.textContent = "Parabéns, você venceu!";
     return true
   } else if (state.values.enemyScore>=state.values.numberOfCards) {
-    console.log("Que pena, você perdeu...")
+    state.view.result.page.style.display = "flex";
+    state.view.result.message.textContent = "Que pena, você perdeu...";
     return true
   } else {
     return false
@@ -264,6 +286,8 @@ function checkComparisonRquirements() {
   comparison()
 }
 async function init() {
+  console.log(state.view.result.page)
+  state.view.result.page.style.display = "none";
   state.values.enemyPokemonList = await cardInHandGenerator();
   createCards(state.values.enemyPokemonList, state.view.cardAreas.enemy, "enemy-card");
   state.values.playerPokemonList = await cardInHandGenerator();
