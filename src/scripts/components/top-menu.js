0cf73fb1caa31ff4c42f9f40fdc861b2e9f139ca
gabriel <dgabrielfof@gmail.com>
2024-01-06 18:06:10 -0300

    feat: adding all new features to the initial project

--- src/scripts/components/top-menu.js
+++ src/scripts/components/top-menu.js
@@ -1,12 +1,9 @@
-import { state } from "../main.js";
+import { handleContent, handlePage, state } from "../main.js";
 
 const localState = {
   icons: {
     profile: '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M224,128a95.76,95.76,0,0,1-31.8,71.37A72,72,0,0,0,128,160a40,40,0,1,0-40-40,40,40,0,0,0,40,40,72,72,0,0,0-64.2,39.37h0A96,96,0,1,1,224,128Z" opacity="0.2"></path><path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24ZM74.08,197.5a64,64,0,0,1,107.84,0,87.83,87.83,0,0,1-107.84,0ZM96,120a32,32,0,1,1,32,32A32,32,0,0,1,96,120Zm97.76,66.41a79.66,79.66,0,0,0-36.06-28.75,48,48,0,1,0-59.4,0,79.66,79.66,0,0,0-36.06,28.75,88,88,0,1,1,131.52,0Z"></path></svg>',
     menu: '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M228,128a12,12,0,0,1-12,12H40a12,12,0,0,1,0-24H216A12,12,0,0,1,228,128ZM40,76H216a12,12,0,0,0,0-24H40a12,12,0,0,0,0,24ZM216,180H40a12,12,0,0,0,0,24H216a12,12,0,0,0,0-24Z"></path></svg>',
-  },
-  view: {
-    container: document.querySelector(".container"),
   },
   profileMenu: [
     { name: "ver perfil", page: "profile"},
@@ -21,32 +18,29 @@ const localState = {
     },
     { name: "configurações", page: "configs"},
   ],
-  menuItems: [
-    { name: "item 1", page: "pageOne" },
-    { name: "item 2", page: "pageTwo" },
-    { name: "item 3", page: "pageThree" },
-  ],
 }
 
 function handleMenu () {
   const menu = document.querySelector(".list-menu");
-  let listMenu = document.querySelector(".sub-list-menu");
-  if (listMenu) {
-    listMenu.remove();
-  } else {
-    listMenu = document.createElement("div");
+  const { listIsOpen, profileIsOpen } = cleanMenus()
+
+  if (!listIsOpen) {
+    const listMenu = document.createElement("div");
     listMenu.classList.add("sub-list-menu");
-    localState.menuItems.forEach( item => {
+    for (let key in state.contents) {
       const element = document.createElement("button");
+      element.disabled = (key === state.content);
       element.classList.add("menu-item");
-      element.textContent = item.name;
+      element.textContent = state.contents[key].span;
+      element.addEventListener("click", () => cleanMenus())
+      element.addEventListener("click", () => handleContent(key))
       listMenu.appendChild(element);
-    });
+    };
     menu.appendChild(listMenu);
-  };
+  }
 }
 
-function confirmLogoutWindow (handlePage) {
+function confirmLogoutWindow () {
   const floatingWindow = document.createElement("div");
   floatingWindow.classList.add("central-container")
 
@@ -71,22 +65,20 @@ function confirmLogoutWindow (handlePage) {
   cancel.textContent = "cancelar";
   cancel.addEventListener("click", () => {
     const logoutWindow = document.querySelector(".central-container")
-    logoutWindow.remove()
+    logoutWindow.remove();
   })
   buttonArea.appendChild(cancel);
 
   floatingWindow.appendChild(buttonArea);
 
-  localState.view.container.appendChild(floatingWindow)
+  state.view.container.appendChild(floatingWindow)
 }
 
-function handleProfileSubMenu (handlePage) {
+function handleProfileSubMenu () {
   const profileMenu = document.querySelector(".profile-menu");
-  let subMenu = document.querySelector(".sub-profile-menu")
-  if (subMenu) {
-    subMenu.remove();
-  } else {
-    subMenu = document.createElement("div");
+  const { listIsOpen, profileIsOpen } = cleanMenus();
+  if (!profileIsOpen) {
+    const subMenu = document.createElement("div");
     subMenu.classList.add("sub-profile-menu");
     localState.profileMenu.forEach( item => {
       const element = document.createElement("div");
@@ -112,16 +104,33 @@ function handleProfileSubMenu (handlePage) {
     logoutSpan.textContent = "sair";
     logout.appendChild(logoutSpan);
     logout.addEventListener("click", () => {
-      confirmLogoutWindow(handlePage)
+      confirmLogoutWindow()
     });
     subMenu.appendChild(logout);
     profileMenu.appendChild(subMenu);
-  };
+  }
 }
 
-export function topMenu(handlePage) {
-  localState.view.container.innerHTML = "";
-  localState.view.container.classList.add("display-flex-col");
+function cleanMenus() {
+  let listIsOpen = false;
+  let profileIsOpen = false;
+  
+  const listMenu = document.querySelector(".sub-list-menu");
+  if (listMenu) {
+    listMenu.remove();
+    listIsOpen = true;
+  }
+  const profileMenu = document.querySelector(".sub-profile-menu")
+  if (profileMenu) {
+    profileMenu.remove();
+    profileIsOpen = true;
+  }
+  return { listIsOpen, profileIsOpen }
+}
+
+export function topMenu() {
+  state.view.container.innerHTML = "";
+  state.view.container.classList.add("display-flex-col");
 
   const topMenu = document.createElement("div");
   topMenu.classList.add("top-menu");
@@ -136,7 +145,7 @@ export function topMenu(handlePage) {
   topMenu.appendChild(listMenu);
   
   const profileMenu = document.createElement("div");
-  profileMenu.addEventListener("click", () => handleProfileSubMenu(handlePage))
+  profileMenu.addEventListener("click", () => handleProfileSubMenu())
   profileMenu.classList.add("profile-menu");  
   const nameUser = document.createElement("span");
   nameUser.textContent = state.user.name;
@@ -144,5 +153,5 @@ export function topMenu(handlePage) {
   profileMenu.innerHTML += localState.icons.profile;  
   topMenu.appendChild(profileMenu);
 
-  localState.view.container.appendChild(topMenu);
+  state.view.container.appendChild(topMenu);
 }
