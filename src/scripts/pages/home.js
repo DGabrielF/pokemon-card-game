cba806f1548415df3a93ab93991d60efbd4f52f2
gabriel <dgabrielfof@gmail.com>
2024-01-06 17:55:28 -0300

    feat:finish alpha homepage

--- src/scripts/pages/home.js
+++ src/scripts/pages/home.js
@@ -1,31 +1,6 @@
-const state = {
-  contents: {
-    getCard: {
-      id: "get-card",
-      span: "pegar cartas",
-      image: "./src/images/add-card.png",
-    },
-    selectCard: { 
-      id: "select-cards",
-      span: "suas sartas",
-      image: "./src/images/set-card.png",
-    },
-    searchDuel: {
-      id: "search-duel",
-      span: "duelar",
-      image: "./src/images/versus.png",
-    },
-    ranking: {
-      id: "ranking",
-      span: "ranking",
-      image: "./src/images/add-card.png",
-    },
-  },
-}
+import { handleContent, state } from "../main.js";
 
-export function homePage (handleContent) {
-  const container = document.querySelector(".container");
-
+export function homePage () {
   let content = document.querySelector(".content");
   if (content) {
     content.innerHTML = "";
@@ -38,23 +13,25 @@ export function homePage (handleContent) {
   contentHomepage.classList.add("homepage");
 
   for (let key in state.contents) {
-    const mainDiv = document.createElement("div");
-    mainDiv.id = state.contents[key].id;
-    mainDiv.addEventListener("click", () => handleContent(key))
-    
-    const divImg = document.createElement("div");
-    const image = document.createElement("img");
-    image.src = state.contents[key].image;
-    divImg.appendChild(image);
-    mainDiv.appendChild(divImg);
-
-    const span = document.createElement("span");
-    span.textContent = state.contents[key].span;
-    mainDiv.appendChild(span);
-
-    contentHomepage.appendChild(mainDiv);
+    if (key !== "homePage") {
+      const mainDiv = document.createElement("div");
+      mainDiv.id = state.contents[key].id;
+      mainDiv.addEventListener("click", () => handleContent(key))
+      
+      const divImg = document.createElement("div");
+      const image = document.createElement("img");
+      image.src = state.contents[key].image;
+      divImg.appendChild(image);
+      mainDiv.appendChild(divImg);
+  
+      const span = document.createElement("span");
+      span.textContent = state.contents[key].span;
+      mainDiv.appendChild(span);
+  
+      contentHomepage.appendChild(mainDiv);
+    }
   }
 
   content.appendChild(contentHomepage);
-  container.appendChild(content);
+  state.view.container.appendChild(content);
 }
